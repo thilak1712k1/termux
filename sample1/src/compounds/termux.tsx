d4e21a08b19e29c3a34376b0adc282da333cde99
Rajan <rajarajan2k1@gmail.com>
2024-01-02 20:16:04 +0530

    first page

---
+++ sample1/src/compounds/termux.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Termux() {
+    return (
+        <>
+            <div>
+                <h1 className="text-6xl font-bold bg-blue-500">
+                    test simple react app web server using termux in mobile
+                </h1>
+            </div>
+        </>
+    )
+}
+
+export default Termux;
